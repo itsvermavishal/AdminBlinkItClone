301bae346e174c47edd1b2c18917464675cd50be
Vishal Verma <vishalpbh022@gmail.com>
2025-10-10 07:46:37 +0530

    Added Splash, SignIn and OTP file Layout

---
+++ app/src/main/java/com/example/adminblinkitclone/viewmodel/AuthViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.adminblinkitclone.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class AuthViewModel : ViewModel(){
+
+    private val _isCurrentUser = MutableStateFlow(false)
+    val isCurrentUser = _isCurrentUser
+
+    init {
+        Utils.getAuthInstance().currentUser?.let {
+            _isCurrentUser.value = true
+        }
+    }
+}
