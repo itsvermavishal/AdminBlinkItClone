b52792408f03c843bb92a9d1d643b0fc4bbc2d43
Vishal Verma <vishalpbh022@gmail.com>
2025-11-22 22:32:20 +0530

    building

--- app/src/main/java/com/example/adminblinkitclone/Contants.kt
+++ app/src/main/java/com/example/adminblinkitclone/Contants.kt
@@ -55,5 +55,28 @@ object Contants {
         "Maida & Sooji"
     )
 
+    val allProductCategoryIcon = arrayOf(
+        R.drawable.vegetable,
+        R.drawable.dairy_breakfast,
+        R.drawable.munchies,
+        R.drawable.cold_and_juices,
+        R.drawable.instant,
+        R.drawable.tea,
+        R.drawable.bakery_biscuits,
+        R.drawable.sweet_tooth,
+        R.drawable.atta_rice,
+        R.drawable.masala,
+        R.drawable.sauce_spreads,
+        R.drawable.chicken_meat,
+        R.drawable.paan_corner,
+        R.drawable.organic_premium,
+        R.drawable.baby,
+        R.drawable.pharma_wellness,
+        R.drawable.cleaning,
+        R.drawable.home_office,
+        R.drawable.personal_care,
+        R.drawable.pet_care,
+    )
+
 
 }
---
+++ app/src/main/java/com/example/adminblinkitclone/adapter/CategoriesAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.adminblinkitclone.adapter
+
+class CategoriesAdapter {
+}
